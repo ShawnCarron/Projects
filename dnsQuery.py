5f1c26a27752e7bf6b5f856d757dade3756e5b29
Shawn Carron <shawn.carron@gmail.com>
2021-10-22 22:33:12 -0400

    Update dnsQuery.py

--- dnsQuery.py
+++ dnsQuery.py
@@ -3,9 +3,10 @@
   
   Author:     Shawn Carron
   Email:      shawn.carron@gmail.com
-  Modified:   2021-10-16
+  Modified:   2021-10-22
   
 """
+import pyperclip
 import PySimpleGUI as sg
 import dns.resolver
 import socket
@@ -16,11 +17,15 @@ import whois
 # Set Theme
 sg.theme("DefaultNoMoreNagging")
 
+# Right click Menu
+
+right_click_menu = ['', ['Paste']]
+
 # Set Layout
 layout = [
     [
         sg.Text("Domain Name: "), sg.InputText(key="-getHost-", background_color="LightGrey", 
-        justification="left", text_color="black", default_text="",),
+        justification="left", text_color="black", default_text="", right_click_menu=right_click_menu),
     ],
     [
         sg.Text("Select Lookup:"),
@@ -28,35 +33,33 @@ layout = [
         sg.Radio("Whois", "RADIO1", key="-radwhois-"),
         sg.Radio("MX Records", "RADIO1", key="-radMX-"),
         sg.Radio("Full", "RADIO1", key="-radFull-", default=True),
+        
     ],
-    # [
-    #     sg.Text("Select DNS:"),
-    #     sg.Radio("TekSavvy DNS", "RADIO2", key="-radtek-", default=True),
-    #     sg.Radio("Google DNS", "RADIO2", key="-radgoogle-"),
-    # ],
-    [sg.Submit("Submit"), sg.Button("Clear"), sg.Button("Exit")],
+
+    [sg.Submit("Submit", button_color="Blue"), sg.Button("Clear", button_color="Blue"), sg.Button("Exit",)],
     [
         sg.Multiline(size=(80, 30), justification="l", key="-textbox-", pad=(10, 10, (10, 10)), 
         background_color="LightGrey", disabled=False, text_color="black",)
     ],
 ]
 
-# Create Window
+# Create Main Window
 window = sg.Window("DNS Tools", layout, size=(500, 400), resizable=False)
 
 # ------------------------FUNCTIONS------------------------##
 
 def host_lookup():
+    # Get the IP address for the domain.
     try:
-        # Get the IP address for the domain.
         hostName = values["-getHost-"]
         getIPAddr = socket.gethostbyname(f"{hostName}")
         # Reverse IP lookup.
         ptr = socket.gethostbyaddr(getIPAddr)[0]
         window["-textbox-"].print(f"The IP for {hostName} is: {getIPAddr}. \nThis domain is hosted on: {ptr}\n ")
     except Exception as err:
-        window["-textbox-"].update(err)
+        window["-textbox-"].print("The DNS query name does not exist: ", err)
 
+##----------------------------------------------------------------##
 
 def mx_lookup():
     # Get the MX records.
@@ -69,6 +72,7 @@ def mx_lookup():
     except Exception as err:
         window["-textbox-"].update(err)
 
+##----------------------------------------------------------------##
 
 def whois_lookup():
     # Get Whois data
@@ -86,8 +90,9 @@ def whois_lookup():
             window["-textbox-"].print("Registrar: ", w.registrar, "\nExpiration Date: ", w.expiration_date, "\n",)
             window["-textbox-"].print("Name Servers:\n", ", ".join(w.name_servers[0:2]).lower())
     except Exception as err:
-        window["-textbox-"].update(err)
+        window["-textbox-"].print("The DNS query name does not exist: ", err)
 
+##----------------------------------------------------------------##
 
 def full_lookup():
     try:
@@ -100,22 +105,19 @@ def full_lookup():
             f"The IP for {hostName} is: {getIPAddr}. \nThis domain is hosted on: {ptr}\n "
         )
     except Exception as err:
-        window["-textbox-"].update(err)
+        window["-textbox-"].print("The DNS query name does not exist: ", err)
         window["-textbox-"].print("")
-    except Exception as err:
+    try:
         # Getting the MX Records.
-        result = dns.resolver.resolve(
-            hostName,
-            "MX",
-        )
+        result = dns.resolver.resolve(hostName, "MX",)
         window["-textbox-"].print(f"The MX Records for {hostName} are:")
         for rdata in result:
             window["-textbox-"].print(rdata.exchange)
-        window["-textbox-"].print(err)
-        window["-textbox-"].print("")
-
-        # Whois Section
+    except Exception as err:
+           window["-textbox-"].print(err)
+           window["-textbox-"].print("")   
     try:
+        # Whois Section
         w = whois.whois(hostName)
         tld = hostName.split(".")[-1]
         if tld == "ca":
@@ -129,7 +131,7 @@ def full_lookup():
             window["-textbox-"].print("Name Servers:\n", ", ".join(w.name_servers[0:2]).lower()
             )
     except Exception as err:
-        window["-textbox-"].print(err)
+        window["-textbox-"].print("The DNS query name does not exist: ", err)
 
 
 def clear_screen():
@@ -137,12 +139,29 @@ def clear_screen():
 
 
 ##---------------------EVENT LOOP---------------------##
+
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == "Exit":
         break
+
+ ##-----------------Right Click Menu Start-----------------##   
+
+    if event in right_click_menu[1]:
+        try:                        # Delete selection before paste
+            window['-getHost-'].Widget.delete("sel.first", "sel.last")
+        except:                     # Exception if no selection
+            pass
+        pasteText = pyperclip.paste()    # Paste into Input field -getHost-
+        window['-getHost-'].Widget.insert("insert", pasteText)
+    else:
+        pass
+        
+
+##-----------------Right Click Menu End-------------------##
+
     if event == "Submit" and values["-getHost-"] == "":
-        window["-textbox-"].update("Domain Name field cannot be blank, enter a domain name and try again.")
+        window["-textbox-"].update("Domain Name field cannot be blank. Please enter a domain name and try again.")
         pass
     elif event == "Clear":
         window["-getHost-"].update("")
