a5ccf3459ad4385e9af4706e25d98b66e6302981
Shawn Carron <shawn.carron@gmail.com>
2021-11-22 22:36:52 -0500

    Update webScrapePDF.py

--- webScrapePDF.py
+++ webScrapePDF.py
@@ -3,10 +3,12 @@ from bs4 import BeautifulSoup as bs
 
 def getPDFs(url, folder):
     
+    # gather html and make pretty
     data = requests.get(url)
     soup = bs(data.text, 'html.parser')
     links = soup.find_all('a')
     
+    #loop through html and grab pdf links
     i = 0
     for link in links:
         if ('.pdf' in link.get('href', [])):
