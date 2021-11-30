377f285cab632632424c851654f687e2e04ffdfc
Shawn Carron <shawn.carron@gmail.com>
2021-11-29 22:36:35 -0500

    Update spaceman.py

--- spaceman.py
+++ spaceman.py
@@ -1,13 +1,24 @@
 import requests
 
-url = 'http://api.open-notify.org/astros.json'
+def inSpace():
+    """
+    Takes no parameters.
+    prints out a list of the astronaughts in space and which spacestation they are on.
+    """
+    url = 'http://api.open-notify.org/astros.json'
 
-r = requests.get(url)
-space = r.json()
-num = len(space['people'])
+    r = requests.get(url)
+    space = r.json()
+    num = len(space['people'])
 
-print(f'The are {num} people currently in space, they are:')
-for people in space['people']:
-    name = people['name']
-    craft = people['craft']
-    print(name + ', on the', craft + '.')
+    print(f'The are {num} people currently in space, they are:')
+    for people in space['people']:
+        name = people['name']
+        craft = people['craft']
+        print(name + ' -->', craft + '.')
+
+def main():
+    inSpace()
+
+if __name__ == '__main__':
+    main()
